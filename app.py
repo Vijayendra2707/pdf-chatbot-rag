136d244e797df783d4e79bcdad47b4764df5a07a
Vijayendra2707 <ranevijayendra@gmail.com>
2026-07-06 21:45:24 +0530

    Updated app main and rag for less memory

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ import requests
 import streamlit as st
 
 
-API_URL = os.getenv(
-    "API_URL",
-    "http://127.0.0.1:8000"
-)
+API_URL = "https://pdf-chatbot-rag-y1lv.onrender.com"
 
 st.set_page_config(
     page_title="PDF RAG Chatbot",
