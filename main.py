91aef88f44201220d935fedcc41a208ed5069165
Vijayendra2707 <ranevijayendra@gmail.com>
2026-08-29 11:43:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -222,7 +222,7 @@ ANSWER:
 
         response = client.chat.completions.create(
 
-            model="llama-3.1-8b-instant",
+            model="llama-3.3-70b-versatile",
 
             messages=[
                 {
