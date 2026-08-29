adbc3b2d7c906f96b16e007ff5f1ca3a76bcf251
Vijayendra2707 <ranevijayendra@gmail.com>
2026-08-29 11:48:20 +0530

    Model changed.py

--- main.py
+++ main.py
@@ -222,7 +222,7 @@ ANSWER:
 
         response = client.chat.completions.create(
 
-            model="llama-3.3-70b-versatile",
+            model="openai/gpt-oss-120b",
 
             messages=[
                 {
